9fd74ef2f1d28c9573649a1e30acd7846b631f98
tristanpendergrass <tristanpendergrass@gmail.com>
2021-08-16 22:11:00 -0700

    Make dice slide up

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,8 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Events
+import Ease exposing (Easing)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -12,16 +14,42 @@ main =
 
 
 
+-- DiceRollAnimation
+
+
+bezierSlideFn : Easing
+bezierSlideFn =
+    Ease.bezier 0.02 0.01 1 -0.53
+
+
+bezierSlideFn2 : Easing
+bezierSlideFn2 =
+    Ease.bezier 0.2 0.1 1 -0.61
+
+
+bezierSlideFn3 : Easing
+bezierSlideFn3 =
+    Ease.bezier 0.02 0.01 0.93 -0.53
+
+
+type DiceRoll
+    = SlideUp Float Float
+    | SpinAnimation
+    | Finished
+
+
+
 -- MODEL
 
 
-type alias Model =
-    String
+type Model
+    = WaitingOnUser
+    | Rolling DiceRoll
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "Why hello there", Cmd.none )
+    ( WaitingOnUser, Cmd.none )
 
 
 
@@ -29,14 +57,31 @@ init _ =
 
 
 type Msg
-    = HandleThingInput String
+    = HandleAnimationFrameDelta Float
+    | Roll
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
+update msg model =
+    let
+        noOp =
+            ( model, Cmd.none )
+    in
     case msg of
-        HandleThingInput str ->
-            ( str, Cmd.none )
+        Roll ->
+            ( Rolling (SlideUp 1000 0), Cmd.none )
+
+        HandleAnimationFrameDelta delta ->
+            case model of
+                Rolling (SlideUp length time) ->
+                    if time >= length then
+                        ( Rolling SpinAnimation, Cmd.none )
+
+                    else
+                        ( Rolling (SlideUp length (time + delta)), Cmd.none )
+
+                _ ->
+                    noOp
 
 
 
@@ -45,26 +90,49 @@ update msg _ =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    Browser.Events.onAnimationFrameDelta HandleAnimationFrameDelta
 
 
 
 -- VIEW
 
 
-renderDiceArea : Html Msg
-renderDiceArea =
-    div [ class "w-64 h-64 bg-gray-700 rounded-lg border-black" ]
+renderDiceArea : Model -> Html Msg
+renderDiceArea model =
+    let
+        renderDie : String -> Easing -> Float -> Html Msg
+        renderDie imageUrl easingFn distance =
+            case model of
+                Rolling (SlideUp duration time) ->
+                    let
+                        percentDone =
+                            time / duration
+
+                        visualPercentDone =
+                            easingFn percentDone
+
+                        topPx =
+                            -1 * visualPercentDone * distance
+
+                        topPxStyle =
+                            String.fromFloat topPx ++ "px"
+                    in
+                    img [ class "w-8 h-8", src imageUrl, class "absolute left-0 right-0", style "top" (Debug.log "topPx" topPxStyle) ] []
+
+                _ ->
+                    img [ class "w-8 h-8", src imageUrl ] []
+    in
+    div [ class "w-64 h-64 bg-gray-700 rounded-lg border-black overflow-hidden" ]
         [ div [ class "w-full h-full flex flex-col justify-evenly" ]
-            [ div [ class "flex w-full justify-evenly" ]
-                [ img [ class "w-8 h-8", src "Dice-1-b.svg" ] []
-                , img [ class "w-8 h-8", src "Dice-2-b.svg" ] []
-                , img [ class "w-8 h-8", src "Dice-3-b.svg" ] []
+            [ div [ class "flex w-full justify-evenly relative" ]
+                [ div [ class "relative w-8 h-8" ] [ renderDie "Dice-1-b.svg" bezierSlideFn 500 ]
+                , div [ class "relative w-8 h-8" ] [ renderDie "Dice-2-b.svg" bezierSlideFn2 500 ]
+                , div [ class "relative w-8 h-8" ] [ renderDie "Dice-3-b.svg" bezierSlideFn3 500 ]
                 ]
-            , div [ class "flex w-full justify-evenly" ]
-                [ img [ class "w-8 h-8", src "Dice-4-b.svg" ] []
-                , img [ class "w-8 h-8", src "Dice-5-b.svg" ] []
-                , img [ class "w-8 h-8", src "Dice-6a-b.svg" ] []
+            , div [ class "flex w-full justify-evenly relative" ]
+                [ div [ class "relative w-8 h-8" ] [ renderDie "Dice-4-b.svg" bezierSlideFn 300 ]
+                , div [ class "relative w-8 h-8" ] [ renderDie "Dice-5-b.svg" bezierSlideFn2 300 ]
+                , div [ class "relative w-8 h-8" ] [ renderDie "Dice-6a-b.svg" bezierSlideFn3 300 ]
                 ]
             ]
         ]
@@ -75,8 +143,12 @@ view model =
     div [ class "w-screen h-screen p-6 bg-gray-900 text-gray-100 flex space-x-4" ]
         [ div [ class "w-64 flex-col justify-center items-center space-y-4" ]
             [ div [ class "flex justify-center w-full" ]
-                [ button [ class "bg-blue-800 hover:bg-blue-700 active:bg-blue-600 cursor-pointer rounded shadow py-1 px-4" ] [ text "Roll" ]
+                [ button
+                    [ class "bg-blue-800 hover:bg-blue-700 active:bg-blue-600 cursor-pointer rounded shadow py-1 px-4"
+                    , onClick Roll
+                    ]
+                    [ text "Roll" ]
                 ]
-            , renderDiceArea
+            , renderDiceArea model
             ]
         ]
